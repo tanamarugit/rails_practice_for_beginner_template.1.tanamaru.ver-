e181d706c7938a4f8552597e23e1931148b5e118
tanamaru <yt.0408.mmm@gmail.com>
2022-12-30 00:31:32 +0900

    ユーザー新規登録機能、ユーザー一覧画面

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+  
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path, success: '登録が完了しました。'
+    else
+      flash.now[:danger] = '登録に失敗しました。'
+      render :new
+    end
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+end
